368c191b7478741fb323574d16a574732e8681b8
Simone Ruffini <simone.ruffini.work@gmail.com>
2024-12-20 14:36:08 +0100

    feat: platform agnostic

--- ili9488.c
+++ ili9488.c
@@ -22,8 +22,8 @@
 /*---------- Private variables -----------------------------------------------*/
 
 /*---------- Private function prototypes -------------------------------------*/
-void ILI9488_swap_int(unsigned int *num1, unsigned int *num2);
-void ILI9488_init_command_list(void);
+static void ILI9488_swap_int(uint16_t *num1, uint16_t *num2);
+static enum ILI9488_Status ILI9488_init_command_list(struct ILI9488_Handle *handle);
 /*---------- Exported Variables ----------------------------------------------*/
 
 /*---------- Exported Functions ----------------------------------------------*/
@@ -31,146 +31,178 @@ void ILI9488_init_command_list(void);
 /*---------- Private Functions -----------------------------------------------*/
 
 /*
- * Writes a byte to SPI without changing chip select (CS) state.
- */
-void ILI9488_spi_send(unsigned char data)
-{
-    HAL_SPI_Transmit(&hspi3, &data, 1, 10);
-    while (HAL_SPI_GetState(&hspi3) != HAL_SPI_STATE_READY)
-        ;
-}
-
-/*
  * Writes a data byte to the display. Pulls CS low as required.
  */
-void ILI9488_write_data(unsigned char data)
-{
-    HAL_GPIO_WritePin(LCD_TFT_DC_GPIO_Port, LCD_TFT_DC_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(LCD_TFT_CS_GPIO_Port, LCD_TFT_CS_Pin, GPIO_PIN_RESET);
-    ILI9488_spi_send(data);
-    HAL_GPIO_WritePin(LCD_TFT_CS_GPIO_Port, LCD_TFT_CS_Pin, GPIO_PIN_SET);
+//void ILI9488_write_data(unsigned char data) {
+enum ILI9488_Status ILI9488_write_data(struct ILI9488_Handle *handle, uint8_t data) {
+    enum ILI9488_Status ret = Status_OK;
+    handle->DC_RS_SetState(PinState_Set);
+    handle->CS_SetState(PinState_Reset);
+    ret = handle->SPI_Transmit(&data, 1);
+    handle->CS_SetState(PinState_Set);
+    return ret;
 }
 
 /*
  * Writes a command byte to the display
  */
-void ILI9488_write_command(unsigned char data)
-{
-    HAL_GPIO_WritePin(LCD_TFT_DC_GPIO_Port, LCD_TFT_DC_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LCD_TFT_CS_GPIO_Port, LCD_TFT_CS_Pin, GPIO_PIN_RESET);
-    ILI9488_spi_send(data);
-    HAL_GPIO_WritePin(LCD_TFT_CS_GPIO_Port, LCD_TFT_CS_Pin, GPIO_PIN_SET);
+enum ILI9488_Status ILI9488_write_command(struct ILI9488_Handle *handle, uint8_t cmd) {
+    enum ILI9488_Status ret = Status_OK;
+    handle->DC_RS_SetState(PinState_Reset);
+    handle->CS_SetState(PinState_Reset);
+    ret = handle->SPI_Transmit(&cmd, 1);
+    handle->CS_SetState(PinState_Set);
+    return ret;
 }
 
 /*
  * Swaps two 16-bit integers
  */
-void ILI9488_swap_int(unsigned int *num1, unsigned int *num2)
-{
+static void ILI9488_swap_int(uint16_t *num1, uint16_t *num2) {
     int temp = *num2;
-    *num2 = *num1;
-    *num1 = temp;
+    *num2    = *num1;
+    *num1    = temp;
 }
 
 /*
  * Same as above, but initialises with an SPI port instead.
  */
-void ILI9488_init()
-{
-    // SET control pins for the LCD HIGH (they are active LOW)
-    HAL_GPIO_WritePin(LCD_TFT_RST_GPIO_Port, LCD_TFT_RST_Pin, GPIO_PIN_SET); // RESET pin HIGH (Active LOW)
-    HAL_GPIO_WritePin(LCD_TFT_CS_GPIO_Port, LCD_TFT_CS_Pin, GPIO_PIN_SET);   // Chip Select Active LOW
-    HAL_GPIO_WritePin(LCD_TFT_DC_GPIO_Port, LCD_TFT_DC_Pin, GPIO_PIN_SET);   // Data / Command select Active LOW
-    HAL_GPIO_WritePin(LCD_TFT_RST_GPIO_Port, LCD_TFT_RST_Pin, GPIO_PIN_RESET);
-    HAL_Delay(120);
-    HAL_GPIO_WritePin(LCD_TFT_RST_GPIO_Port, LCD_TFT_RST_Pin, GPIO_PIN_SET);
-    HAL_Delay(120);
+#include <stddef.h>
+enum ILI9488_Status ILI9488_init(struct ILI9488_Handle *handle) {
+    if (handle == NULL)
+        return Status_ERR;
 
-    ILI9488_init_command_list();
+    if (handle->CS_SetState == NULL || handle->DC_RS_SetState == NULL || handle->RST_SetState == NULL ||
+        handle->SPI_Transmit == NULL || handle->Delay == NULL)
+        return Status_ERR;
+
+    // SET control pins for the LCD HIGH (they are active LOW)
+    handle->RST_SetState(PinState_Set);    // RESET pin HIGH (Active LOW)
+    handle->CS_SetState(PinState_Set);     // Chip Select Active LOW
+    handle->DC_RS_SetState(PinState_Set);  // Data / Command select Active LOW
+    handle->RST_SetState(PinState_Reset);
+    handle->Delay(120);
+    handle->RST_SetState(PinState_Set);
+    handle->Delay(120);
+
+    return ILI9488_init_command_list(handle);
 }
 
 /**
  * This is the magic initialisation routine.
  */
-void ILI9488_init_command_list(void)
-{
+static enum ILI9488_Status ILI9488_init_command_list(struct ILI9488_Handle *handle) {
+    uint8_t tmp = 0;
     //********Start Initial Sequence*******//
-    ILI9488_write_command(0xE0); // P-Gamma
-    ILI9488_write_data(0x00);
-    ILI9488_write_data(0x13);
-    ILI9488_write_data(0x18);
-    ILI9488_write_data(0x04);
-    ILI9488_write_data(0x0F);
-    ILI9488_write_data(0x06);
-    ILI9488_write_data(0x3A);
-    ILI9488_write_data(0x56);
-    ILI9488_write_data(0x4D);
-    ILI9488_write_data(0x03);
-    ILI9488_write_data(0x0A);
-    ILI9488_write_data(0x06);
-    ILI9488_write_data(0x30);
-    ILI9488_write_data(0x3E);
-    ILI9488_write_data(0x0F);
-    ILI9488_write_command(0XE1); // N-Gamma
-    ILI9488_write_data(0x00);
-    ILI9488_write_data(0x13);
-    ILI9488_write_data(0x18);
-    ILI9488_write_data(0x01);
-    ILI9488_write_data(0x11);
-    ILI9488_write_data(0x06);
-    ILI9488_write_data(0x38);
-    ILI9488_write_data(0x34);
-    ILI9488_write_data(0x4D);
-    ILI9488_write_data(0x06);
-    ILI9488_write_data(0x0D);
-    ILI9488_write_data(0x0B);
-    ILI9488_write_data(0x31);
-    ILI9488_write_data(0x37);
-    ILI9488_write_data(0x0F);
-    ILI9488_write_command(0xC0);
-    ILI9488_write_data(0x18);
-    ILI9488_write_data(0x16);
-    ILI9488_write_command(0xC1);
-    ILI9488_write_data(0x45);
-    ILI9488_write_command(0xC5); // VCOM
-    ILI9488_write_data(0x00);
-    ILI9488_write_data(0x63);
-    ILI9488_write_data(0x01);
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xE0);  // P-Gamma
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x00);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x13);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x18);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x04);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0F);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x06);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x3A);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x56);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x4D);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x03);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0A);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x06);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x30);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x3E);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0F);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0XE1);  // N-Gamma
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x00);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x13);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x18);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x01);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x11);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x06);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x38);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x34);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x4D);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x06);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0D);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0B);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x31);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x37);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x0F);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xC0);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x18);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x16);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xC1);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x45);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xC5);  // VCOM
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x00);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x63);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x01);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(0x36); // RAM address mode
+    tmp += (uint8_t)ILI9488_write_command(handle, 0x36);  // RAM address mode
     // 0xF8 and 0x3C are landscape mode. 0x5C and 0x9C for portrait mode.
     if (LANDSCAPE)
-        ILI9488_write_data(0xF8);
+        tmp += (uint8_t)ILI9488_write_data(handle, 0xF8);
     else
-        ILI9488_write_data(0x5C);
+        tmp += (uint8_t)ILI9488_write_data(handle, 0x5C);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(0x3A);  // Interface Mode Control
-    ILI9488_write_data(0x66); // 16-bit serial mode
-    ILI9488_write_command(0xB0);  // Interface Mode Control
-    ILI9488_write_data(0x80); // SDO not in use
-    ILI9488_write_command(0xB1);  // Frame rate 70HZ
-    ILI9488_write_data(0x00); //
-    ILI9488_write_data(0x10);
-    ILI9488_write_command(0xB4);
-    ILI9488_write_data(0x02);
+    tmp += (uint8_t)ILI9488_write_command(handle, 0x3A);  // Interface Mode Control
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x66);     // 16-bit serial mode
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xB0);  // Interface Mode Control
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x80);     // SDO not in use
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xB1);  // Frame rate 70HZ
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x00);     //
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x10);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xB4);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x02);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(0xB6); // RGB/MCU Interface Control
-    ILI9488_write_data(0x02);
-    // ILI9488_write_data(0x22);
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xB6);  // RGB/MCU Interface Control
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x02);
+    // tmp += (uint8_t)ILI9488_write_data(handle,0x22);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(0xE9);
-    ILI9488_write_data(0x00);
-    ILI9488_write_command(0xF7);
-    ILI9488_write_data(0xA9);
-    ILI9488_write_data(0x51);
-    ILI9488_write_data(0x2C);
-    ILI9488_write_data(0x82);
-    ILI9488_write_command(0x11);
-    HAL_Delay(120);
-    ILI9488_write_command(0x20); // change it to 0x21 if you want to invert colors
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xE9);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x00);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0xF7);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0xA9);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x51);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x2C);
+    tmp += (uint8_t)ILI9488_write_data(handle, 0x82);
+    if (!tmp)
+        return Status_ERR;
+    tmp += (uint8_t)ILI9488_write_command(handle, 0x11);
+    if (!tmp)
+        return Status_ERR;
+    handle->Delay(120);
+    tmp += (uint8_t)ILI9488_write_command(handle, 0x20);  // change it to 0x21 if you want to invert colors
+    if (!tmp)
+        return Status_ERR;
 
-    HAL_Delay(120);
-    ILI9488_write_command(0x29);
+    handle->Delay(120);
+    tmp += (uint8_t)ILI9488_write_command(handle, 0x29);
+    if (!tmp)
+        return Status_ERR;
+    return Status_OK;
 }
 
 /*
@@ -178,27 +210,35 @@ void ILI9488_init_command_list(void)
  * Should only be called within a function that draws something
  * to the display.
  */
-void ILI9488_set_draw_window(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2)
-{
+enum ILI9488_Status ILI9488_set_draw_window(struct ILI9488_Handle *handle, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
+    uint8_t tmp;
     // Check that the values are in order
     if (x2 < x1)
         ILI9488_swap_int(&x2, &x1);
     if (y2 < y1)
         ILI9488_swap_int(&y2, &y1);
 
-    ILI9488_write_command(ILI9488_CASET);
-    ILI9488_write_data(x1 >> 8);
-    ILI9488_write_data(x1 & 0xFF);
+    tmp += (uint8_t)ILI9488_write_command(handle,ILI9488_CASET);
+    tmp += (uint8_t)ILI9488_write_data(handle,x1 >> 8);
+    tmp += (uint8_t)ILI9488_write_data(handle,x1 & 0xFF);
 
-    ILI9488_write_data(x2 >> 8);
-    ILI9488_write_data(x2 & 0xFF);
+    tmp += (uint8_t)ILI9488_write_data(handle,x2 >> 8);
+    tmp += (uint8_t)ILI9488_write_data(handle,x2 & 0xFF);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(ILI9488_PASET);
-    ILI9488_write_data(y1 >> 8);
-    ILI9488_write_data(y1 & 0xFF);
+    tmp += (uint8_t)ILI9488_write_command(handle,ILI9488_PASET);
+    tmp += (uint8_t)ILI9488_write_data(handle,y1 >> 8);
+    tmp += (uint8_t)ILI9488_write_data(handle,y1 & 0xFF);
 
-    ILI9488_write_data(y2 >> 8);
-    ILI9488_write_data(y2 & 0xFF);
+    tmp += (uint8_t)ILI9488_write_data(handle,y2 >> 8);
+    tmp += (uint8_t)ILI9488_write_data(handle,y2 & 0xFF);
+    if (!tmp)
+        return Status_ERR;
 
-    ILI9488_write_command(ILI9488_RAMWR);
+    tmp += (uint8_t)ILI9488_write_command(handle,ILI9488_RAMWR);
+    if (!tmp)
+        return Status_ERR;
+
+    return Status_OK;
 }
--- ili9488.h
+++ ili9488.h
@@ -15,10 +15,27 @@
 #ifndef _ILI9488_H_
 #define _ILI9488_H_
 
+/* ---------- Includes -------------------------------------------------------*/
+#include "stdint.h"
+
+/* ---------- Exported types -------------------------------------------------*/
+
+enum ILI9488_PinState { PinState_Reset = 0, PinState_Set };
+enum ILI9488_Status { Status_OK = 0, Status_ERR, Status_Busy, Status_Timeout };
+
+struct ILI9488_Handle {
+    void (*CS_SetState)(enum ILI9488_PinState state);
+    void (*DC_RS_SetState)(enum ILI9488_PinState state);
+    void (*RST_SetState)(enum ILI9488_PinState state);
+    enum ILI9488_Status (*SPI_Transmit)(uint8_t *data, uint16_t size);
+    void (*Delay)(uint32_t delay);  // Delay in ms
+};
+
+/* ---------- Exported constants ---------------------------------------------*/
 // Parameter settings
-#define HORIZONTAL_RES  480
-#define VERTICAL_RES    320 
-#define LANDSCAPE       1
+#define HORIZONTAL_RES 480
+#define VERTICAL_RES   320
+#define LANDSCAPE      1
 
 // Registers (DO NOT CHANGE)
 #define ILI9488_NOP     0x00
@@ -26,31 +43,31 @@
 #define ILI9488_RDDID   0x04
 #define ILI9488_RDDST   0x09
 
-#define ILI9488_SLPIN   0x10
-#define ILI9488_SLPOUT  0x11
-#define ILI9488_PTLON   0x12
-#define ILI9488_NORON   0x13
+#define ILI9488_SLPIN  0x10
+#define ILI9488_SLPOUT 0x11
+#define ILI9488_PTLON  0x12
+#define ILI9488_NORON  0x13
 
-#define ILI9488_RDMODE  0x0A
-#define ILI9488_RDMADCTL  0x0B
-#define ILI9488_RDPIXFMT  0x0C
-#define ILI9488_RDIMGFMT  0x0D
-#define ILI9488_RDSELFDIAG  0x0F
+#define ILI9488_RDMODE     0x0A
+#define ILI9488_RDMADCTL   0x0B
+#define ILI9488_RDPIXFMT   0x0C
+#define ILI9488_RDIMGFMT   0x0D
+#define ILI9488_RDSELFDIAG 0x0F
 
-#define ILI9488_INVOFF  0x20
-#define ILI9488_INVON   0x21
+#define ILI9488_INVOFF   0x20
+#define ILI9488_INVON    0x21
 #define ILI9488_GAMMASET 0x26
-#define ILI9488_DISPOFF 0x28
-#define ILI9488_DISPON  0x29
+#define ILI9488_DISPOFF  0x28
+#define ILI9488_DISPON   0x29
 
-#define ILI9488_CASET   0x2A
-#define ILI9488_PASET   0x2B
-#define ILI9488_RAMWR   0x2C
-#define ILI9488_RAMRD   0x2E
+#define ILI9488_CASET 0x2A
+#define ILI9488_PASET 0x2B
+#define ILI9488_RAMWR 0x2C
+#define ILI9488_RAMRD 0x2E
 
-#define ILI9488_PTLAR   0x30
-#define ILI9488_MADCTL  0x36
-#define ILI9488_PIXFMT  0x3A
+#define ILI9488_PTLAR  0x30
+#define ILI9488_MADCTL 0x36
+#define ILI9488_PIXFMT 0x3A
 
 #define ILI9488_FRMCTR1 0xB1
 #define ILI9488_FRMCTR2 0xB2
@@ -58,37 +75,29 @@
 #define ILI9488_INVCTR  0xB4
 #define ILI9488_DFUNCTR 0xB6
 
-#define ILI9488_PWCTR1  0xC0
-#define ILI9488_PWCTR2  0xC1
-#define ILI9488_PWCTR3  0xC2
-#define ILI9488_PWCTR4  0xC3
-#define ILI9488_PWCTR5  0xC4
-#define ILI9488_VMCTR1  0xC5
-#define ILI9488_VMCTR2  0xC7
+#define ILI9488_PWCTR1 0xC0
+#define ILI9488_PWCTR2 0xC1
+#define ILI9488_PWCTR3 0xC2
+#define ILI9488_PWCTR4 0xC3
+#define ILI9488_PWCTR5 0xC4
+#define ILI9488_VMCTR1 0xC5
+#define ILI9488_VMCTR2 0xC7
 
-#define ILI9488_RDID1   0xDA
-#define ILI9488_RDID2   0xDB
-#define ILI9488_RDID3   0xDC
-#define ILI9488_RDID4   0xDD
+#define ILI9488_RDID1 0xDA
+#define ILI9488_RDID2 0xDB
+#define ILI9488_RDID3 0xDC
+#define ILI9488_RDID4 0xDD
 
 #define ILI9488_GMCTRP1 0xE0
 #define ILI9488_GMCTRN1 0xE1
 
-/* ---------- Includes -------------------------------------------------------*/
-#include "main.h"
-
-/* ---------- Exported types -------------------------------------------------*/
-extern SPI_HandleTypeDef hspi3;
-
-/* ---------- Exported constants ---------------------------------------------*/
 /* ---------- Exported variables ---------------------------------------------*/
 /* ---------- Exported macros ------------------------------------------------*/
 /* ---------- Exported functions ---------------------------------------------*/
-void ILI9488_spi_send(unsigned char data);
-void ILI9488_write_data(unsigned char data);
-void ILI9488_write_command(unsigned char data);
-void ILI9488_init();
-void ILI9488_set_draw_window(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2);
+enum ILI9488_Status ILI9488_write_data(struct ILI9488_Handle *handle, uint8_t data);
+enum ILI9488_Status ILI9488_write_command(struct ILI9488_Handle *handle, uint8_t cmd);
+enum ILI9488_Status ILI9488_init(struct ILI9488_Handle *handle);
+enum ILI9488_Status ILI9488_set_draw_window(struct ILI9488_Handle *handle, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
 /* ---------- Private types --------------------------------------------------*/
 /* ---------- Private variables ----------------------------------------------*/
 /* ---------- Private constants ----------------------------------------------*/
